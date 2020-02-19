c3772c7fc1015c7bbd20f4791d33220b70105e4e
Kelsha_Darby <ohanaipo@gmail.com>
2020-02-19 09:54:23 -0700

    Add recommend_exhibits method

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,6 +9,19 @@ class Museum
   end
 
   def add_exhibit(exhibit)
-    @exhibits << exhibit  
+    @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    matching_exhibits = []
+    @exhibits.map do |exhibit|
+      patron.interests.map do |interest|
+        if interest == exhibit.name
+          matching_exhibits << exhibit
+        end
+      end
+    end
+    matching_exhibits
+  end
+
 end
